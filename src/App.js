2ad51cfbee7f6fbc4f6114e615ae998a211b68b0
namtaemin <kingohot@gmail.com>
2024-03-30 13:36:21 +0900

    Change tile

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const name = "REACT"
+  const name = "REACT 1st project"
   return (
     <div className="react">{name}</div>
   );
